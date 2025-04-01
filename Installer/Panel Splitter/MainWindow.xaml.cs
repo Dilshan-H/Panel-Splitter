fdf816faeae4de3528976d7c11510813d1e119e5
dilshan-h <77499497+Dilshan-H@users.noreply.github.com>
2025-04-01 14:38:09 +0530

    Add installer application initial code with UI

---
+++ Installer/Panel Splitter/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Panel_Splitter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void installBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void footerTextBlock_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void updatesCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
